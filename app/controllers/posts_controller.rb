f7b53bb0e9dac95d1ef96e285e89809a27c742fe
Voronko Maksim <voronkomax@gmail.com>
2021-06-22 14:23:04 +0300

    Add actions new, create in Posts Controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,4 +8,18 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(title: "", body: "", user_name: "", picture_url:"")
+    
+    if @post.save
+      redirect_to @post
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 end
