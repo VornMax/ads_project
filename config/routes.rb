d48c4531b4ff48b8ab0aee47cfc12ee0020b7ed8
Voronko Maksim <voronkomax@gmail.com>
2021-06-24 16:35:34 +0300

    Add sessions routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'sessions/new'
   root 'posts#index'
   
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :posts
   resources :users
