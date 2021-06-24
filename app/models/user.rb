1952f73708c2a279249a16ff1c8a6b95a02dbcc9
Voronko Maksim <voronkomax@gmail.com>
2021-06-24 14:30:48 +0300

    Add table surname in table User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ApplicationRecord
-  before_save (self.email = email.downcase)
-  validates :name, presence: true length: { maximum: 50 }
-  validates :surname, presence: true length: { maximum: 50 }
-  VALID_EMAIL_REGEXP = /\A[\w+\-.]+@[a-z\d-.]+\.[a-z]+\z\if condition
+  before_save { self.email = email.downcase }
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :surname, presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 }, 
-                    format: { with: VALID_EMAIL_REGEXP },
+                    format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
-  has_secure_password	
-  end
+  has_secure_password
+  validates :password, length: { minimum: 6 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2021_06_24_095915) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
+    t.string "surname"
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
