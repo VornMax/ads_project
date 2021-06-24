809b973e83b0f248d443bccc1cc0026c8ff6e51d
Voronko Maksim <voronkomax@gmail.com>
2021-06-24 14:51:57 +0300

    Add action show to controller Users, and create resources routes for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'users/new'
   root 'posts#index'
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :posts
+  resources :users
 end
