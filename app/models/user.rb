b9758523b7b42f710500672274865ef7f5fa9f9e
Voronko Maksim <voronkomax@gmail.com>
2021-06-24 13:01:57 +0300

    Add validates in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+  before_save (self.email = email.downcase)
+  validates :name, presence: true length: { maximum: 50 }
+  validates :surname, presence: true length: { maximum: 50 }
+  VALID_EMAIL_REGEXP = /\A[\w+\-.]+@[a-z\d-.]+\.[a-z]+\z\if condition
+  validates :email, presence: true, length: { maximum: 255 }, 
+                    format: { with: VALID_EMAIL_REGEXP },
+                    uniqueness: { case_sensitive: false }
+  has_secure_password	
+  end
 end
