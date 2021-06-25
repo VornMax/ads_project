34ffe46a055123e108bb122358b83c961e305e3f
Voronko Maksim <voronkomax@gmail.com>
2021-06-25 12:10:58 +0300

    Add links in Views

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -6,6 +6,10 @@ module SessionsHelper
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
   end
+  
+  def logged_in?
+    !current_user.nil?
+  end
 
   def log_out
     session.delete(:user_id)
